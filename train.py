52d1c5e9285ac3aab32c96dc3814ed8ee17cedd8
Raldir <rami.dragneel@outlook.com>
2018-05-29 03:01:21 +0200

    Added NN, and summary of understanding of taxi

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from jnt.common import ensure_dir
 from jnt.isas.supervised import SuperTaxi, METHODS
 from traceback import format_exc
 from os.path import join
+import itertools
 import argparse
 
 
@@ -15,6 +16,9 @@ isa_fpaths = [join(RES_DIR,"en_ma.csv.gz")]
 freq_fpaths =[""]
 
 
+def findsubsets(S,m):
+    return set(itertools.combinations(S, m))
+
 def run(output_dir, feature_num, mode):
 
     feature_num = int(feature_num)
@@ -25,24 +29,24 @@ def run(output_dir, feature_num, mode):
     features = ["hyper_in_hypo_i","hypo2hyper_substract", "freq_substract", "in_weight_substract", "length_substract",
             "hypo2hyper_s_substract","hypo2hyper_max2_substract"]
     features = features[:feature_num]
-    
+
     if mode == "gridsearch":
         #  grid search is only supported for SVC
         method = "SVC"
         hc = SuperTaxi(join(output_dir, "SVC-grid-search"), method="SVC", features=features, overwrite=True)
         clf = hc.grid_search_svc(taxo_features.relations, test=TEST_GRIDSEARCH)
-        return 
-    
+        return
+
     for method in METHODS:
         try:
-            classifier_dir = join(output_dir, method) 
+            classifier_dir = join(output_dir, method)
             print "\n", method.upper(), "\n", "="*50
             hc = SuperTaxi(classifier_dir, method=method, features=features, overwrite=True)
             if mode == "train":
                 clf = hc.train(taxo_features.relations)
                 hc._print_clf_info()
             elif mode == "cv":
-                hc.crossval(taxo_features.relations) 
+                hc.crossval(taxo_features.relations)
             else:
                 print "Error: unrecognised mode %s" % mode
         except:
@@ -59,9 +63,7 @@ def main():
     print "Output directory: ", args.output_dir
     print "Features number: ", args.feature_num
     print "Mode: ", args.mode
-    
     run(args.output_dir, args.feature_num, args.mode)
 
 if __name__ == '__main__':
     main()
-
