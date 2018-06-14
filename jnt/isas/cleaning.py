77c108968662bfda6abc8bb58c79ed4533a86b95
Raldir <rami.dragneel@outlook.com>
2018-06-14 14:48:16 +0200

    addded connection to root and removal of noise

--- jnt/isas/cleaning.py
+++ jnt/isas/cleaning.py
@@ -1,7 +1,6 @@
 import os
 import csv
 
-
 def connected_to_root(element, list_data, root):
     parent = element
     parent_last = None
@@ -26,28 +25,40 @@ def zero_out_nodes(elements):
     return zero_out
 
 
-list_data = set([])
+def all_parent_rel(element, list_data):
+    parent_root = element
+    remove_relations = []
+    for relation in list_data:
+        if relation[0] == parent_root:
+            remove_relations.append(relation)
+    for ele in remove_relations:
+        remove_relations += all_parent_rel(ele[1], list_data)
+    return remove_relations
+
+
+list_data = []
 with open(os.path.join(os.path.dirname(__file__), '../../', 'vocabularies', 'science_en.csv-space-relations.csv-taxo.csv-LinearSVC.csv'), 'rb') as f:
     reader = csv.reader(f, delimiter = '\t')
     for i, line in enumerate(reader):
-        list_data.add((line[1], line[2]))
-    ROOT = "science"
+        list_data.append((line[1], line[2]))
+ROOT = "science"
+all_parent_root = all_parent_rel(ROOT, list_data)
+for el in all_parent_root:
+    list_data.remove(el)
 
-    elements = set([])
-    for relation in list_data:
-        elements.add(relation[0])
-        elements.add(relation[1])
-    elements_connected = set([])
-    for element in elements:
-        ele_root = connected_to_root(element, list_data, ROOT)
-        if ele_root[0]:
-            print ele_root[1]
-            elements_connected.add(element)
-        else:
-            print ele_root[1]
+elements = set([])
+for relation in list_data:
+    elements.add(relation[0])
+    elements.add(relation[1])
+elements_connected = set([])
+for element in elements:
+    ele_root = connected_to_root(element, list_data, ROOT)
+    if not ele_root[0]:
+        list_data.append((ele_root[1], ROOT))
 
-    zero_out = zero_out_nodes(list_data)
-    #print zero_out
-    for element in zero_out:
-        list_data.add((element, ROOT))
-    #print list_data
+#find_cycles(ROOT, list_data)
+list_all_childs(ROOT, list_data)
+
+# with open(os.path.join(os.path.dirname(__file__), '../../', 'vocabularies', 'science_en.taxo'), 'wb') as f:
+#     for element in list_data:
+#         f.write(element[0] + '\t' + element[1]  + '\n')
