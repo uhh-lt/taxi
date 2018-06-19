35ba91028cbed88537006e47793f303e1624a69c
monsterwels <untoter_monsterwels@hotmail.de>
2018-06-19 10:40:16 -0700

    Graph-Pruning nach Tarjan

--- graph_pruning/graph_pruning.py
+++ graph_pruning/graph_pruning.py
@@ -2,9 +2,68 @@
 import csv
 import sys
 import random
-
+from tarjan import tarjan
 
 hypo_to_hyper = {}
+hyper_to_hypo = {}
+
+
+def prepare_tarjan(node):
+    id = node[0]
+    hypo = node[1]
+    hyper = node[2]
+
+    print("Read line with ID '%s': %s --> %s" % (id, hypo, hyper))
+
+    if hyper not in hyper_to_hypo:
+        hyper_to_hypo[hyper] = []
+
+    if hypo not in hyper_to_hypo:
+        hyper_to_hypo[hypo] = []
+
+    hyper_to_hypo[hyper].append(hypo)
+
+
+
+def do_tarjan():
+    cycles_removed = 0
+
+    cycle = []  # Initialize with value to trigger the while loop. Python has no do-while...
+
+
+    while(cycle is not None):
+        print("Running tarjan...")
+        t = tarjan(hyper_to_hypo)
+
+        # ['plant pathology','pathology'],
+
+        i = 0
+        cycle = None
+
+        while(cycle is None and i < len(t)):
+            if len(t[i]) > 1:
+                # Do pruning
+                print("Cycle detected: %s" % t[i])
+                cycle = t[i]
+
+            i += 1
+
+        if cycle is not None:
+            hypernym_index_removed_from = random.randint(0, len(cycle) - 1)
+            hypernym_removed_from = cycle[hypernym_index_removed_from]
+
+            for c in cycle:
+                if c in hyper_to_hypo[hypernym_removed_from]:
+                    print("Remove hyponym '%s' from hypernym '%s'." % (c, hypernym_removed_from))
+                    hyper_to_hypo[hypernym_removed_from].remove(c)
+                    cycles_removed += 1
+                    break
+
+
+    print("Removed %s cycles." % cycles_removed)
+    return hyper_to_hypo
+
+
 
 
 def add_line(node):
@@ -31,6 +90,10 @@ def add_line(node):
 filename_in = None
 filename_out = None
 delimiter = '\t'
+mode = "old"
+root = None
+
+supported_modes = ["tarjan", "old"]
 
 if len(sys.argv) >= 2:
     filename_in = sys.argv[1]
@@ -39,8 +102,13 @@ if len(sys.argv) >= 3:
     filename_out = sys.argv[2]
 
 if len(sys.argv) >= 4:
-    delimiter = sys.argv[3]
+    mode = sys.argv[3]
 
+if len(sys.argv) >= 5:
+    root = sys.argv[4]
+
+if len(sys.argv) >= 6:
+    delimiter = sys.argv[5]
 
 if filename_in is None:
     raise Exception("No CSV-file provided")
@@ -48,34 +116,111 @@ if filename_in is None:
 if filename_out is None:
     raise Exception("No output provided")
 
+if mode not in supported_modes:
+    raise Exception("Mode '%s' unknown. Supported: %s" % (mode, supported_modes))
+
 
 print("Reading file: %s" % filename_in)
 print("Delimiter: %s" % delimiter)
+print("Mode: %s" % mode)
+
+if root is not None:
+    print("Root: %s" % root)
 
 with open(filename_in, "r") as f:
     reader = csv.reader(f, delimiter=delimiter)
 
     for i, line in enumerate(reader):
-        add_line(line)
+        if mode == "old":
+            add_line(line)
+        elif mode == "tarjan":
+            prepare_tarjan(line)
 
-print("Relations created (hypo_to_hyper):")
-print(hypo_to_hyper)
+result = None
 
-print("Write result.")
+if mode == "old":
+    print("Relations created (hypo_to_hyper):")
+    print(hypo_to_hyper)
 
-with open(filename_out, "w+") as f:
-    writer = csv.writer(f, delimiter=delimiter)
-    id = 0
+    print("Write result.")
 
-    for hypo in hypo_to_hyper:
-        row = [id, hypo, hypo_to_hyper[hypo]]
+    with open(filename_out, "w+") as f:
+        writer = csv.writer(f, delimiter=delimiter)
+        id = 0
 
-        print("   %s" % row)
-        writer.writerow(row)
+        for hypo in hypo_to_hyper:
+            row = [id, hypo, hypo_to_hyper[hypo]]
 
-        id += 1
+            print("   %s" % row)
+            writer.writerow(row)
+
+            id += 1
+elif mode == "tarjan":
+    result = do_tarjan()
+
+    print("Relations created (tarjan):")
+    print(result)
+
+    print("Write result.")
+
+    # digraph G {
+    #   "Welcome" -> "To"
+    #   "To" -> "Web"
+    #   "To" -> "GraphViz!"
+    # }
+
+    graphviz = "digraph G {\n"
+
+    with open(filename_out, "w+") as f:
+        writer = csv.writer(f, delimiter=delimiter)
+        id = 0
+
+        for hyper in result:
+            if root is not None and len(result[hyper]) == 0:
+                result[hyper].append(root)
+
+            for hypo in result[hyper]:
+                if hypo == hyper:
+                    print("Found hypernym %s equivalent to hyponym %s." % (hyper, hypo))
+                else:
+                    row = [id, hypo, hyper]
+
+                    print("   %s" % row)
+                    writer.writerow(row)
+
+                    graphviz += "\"%s\" -> \"%s\"\n" % (hyper, hypo)
+
+                    id += 1
+
+    graphviz += "}"
 
 
+    with open(filename_out + "_graphviz", "w+") as f:
+        f.write(graphviz)
+
+    graphviz += "}"
 
 
+    print(graphviz)
 
+    with open(filename_out + "_gephi.csv", "w+") as f:
+        writer = csv.writer(f, delimiter=delimiter)
+        writer.writerow(["id", "source", "target"])
+        id = 0
+
+        for hyper in result:
+            if root is not None and len(result[hyper]) == 0:
+                result[hyper].append(root)
+
+            for hypo in result[hyper]:
+                if hypo == hyper:
+                    print("Found hypernym %s equivalent to hyponym %s." % (hyper, hypo))
+                else:
+                    row = [id, hypo, hyper]
+
+                    print("   %s" % row)
+                    writer.writerow(row)
+
+                    graphviz += "\"%s\" -> \"%s\"\n" % (hyper, hypo)
+
+                    id += 1
